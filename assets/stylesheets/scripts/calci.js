65ecdaa30ae3770c03e31ab1bb8979570f88d5eb
Sahiti <sahitigopal20@gmail.com>
2015-09-01 16:28:34 +0530

    Corner cases

--- assets/stylesheets/scripts/calci.js
+++ assets/stylesheets/scripts/calci.js
@@ -4,7 +4,7 @@ var Calci = {
       if(this.dataset.keyType == "digit") {
         Calci.handleInput(this.dataset.digit);
       } else if (this.dataset.keyType == "operator") {
-        Calci.handleInput(this.dataset.operator);
+        Calci.handleOperator(this.dataset.operator);
       } else if (this.dataset.keyType == "delete") {
         Calci.handleDelete();
       } else if (this.dataset.keyType == "equals") {
@@ -12,11 +12,17 @@ var Calci = {
       }
     });
     $('#calculator #delete').dblclick(function() {
+      Calci.clearPreview();
       Calci.clearResult();
     });
-    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '*', '+', '-'].forEach(function(digit) {
+    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].forEach(function(digit) {
       $(document).bind('keyup', digit, function() {
         Calci.handleInput(digit);
+      });
+    });
+    ['/', '*', '+', '-'].forEach(function(digit) {
+      $(document).bind('keyup', digit, function() {
+        Calci.handleOperator(digit);
       });
     });
     $(document).bind('keyup', '.', function() {
@@ -29,14 +35,14 @@ var Calci = {
       }
     });
     $(document).bind('keyup', 'backspace', function() {
-      Calci.handleDelete();
+     Calci.handleDelete();
     });
     $(document).bind('keyup', 'c', function() {
-      $('#result').html('');
-      $('#preview').html('');
+    $('#preview').html('');
+     $('#result').html('');
     });
     $(document).bind('keyup', 'shift+=', function() {
-      Calci.handleInput('+');
+      Calci.handleOperator('+');
     });
     ['=', 'return'].forEach(function(key) {
       $(document).bind('keyup', key, function() {
@@ -46,6 +52,19 @@ var Calci = {
   },
   handleInput: function(input) {
     $('#preview').html($('#preview').html() + input);
+  },
+  handleOperator: function(operator) {
+    if ($('#preview').html().length == 0) {
+      if (operator == '-') {
+        Calci.handleInput('-');
+      }
+    } else {
+      lastChar = Calci.getLastChar();
+      if (['+', '-', '*', '/'].indexOf(lastChar) != -1) {
+        Calci.handleDelete();
+      }
+      Calci.handleInput(operator);
+    }
   },
   handleDelete: function() {
     $('#preview').html($('#preview').html().slice(0, -1));
@@ -59,6 +78,9 @@ var Calci = {
   clearResult: function() {
     $('#result').html('');
   },
+  clearPreview: function() {
+    $('#preview').html('');
+  },
   getLastNumber: function() {
     str = $('#preview').html();
     regexp = /[+\-*\/]([0-9.])*$/
@@ -67,6 +89,14 @@ var Calci = {
       return str;
     } else {
       return matches[0].slice(1);
+    }
+  },
+  getLastChar: function() {
+    str = $('#preview').html();
+    if (str.length == 0) {
+      return str;
+    } else {
+      return str[str.length - 1];
     }
   }
 };
